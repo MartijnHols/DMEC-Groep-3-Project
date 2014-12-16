ae958dc39888681254b90d9b645ddb1662f8055c
Martijn Hols <martijnhols@hotmail.com>
2014-12-16 12:54:48 +0100

    Add groepsgenoten

--- js/expeditie.js
+++ js/expeditie.js
@@ -5,14 +5,51 @@ app.config(['$routeProvider', function ($routeProvider) {
 		templateUrl: 'templates/iets.html',
 		controller: 'iets'
 	}).otherwise({
-		templateUrl: 'templates/expeditie/expeditie.html',
-		controller: 'expeditie'
+		templateUrl: 'templates/expeditie/groepsgenoten.html',
+		controller: 'groepsgenoten'
 	});
 }]);
 
 app.controller('main', function ($scope) {
 
 });
-app.controller('expeditie', function ($scope) {
-
+app.controller('groepsgenoten', function ($scope) {
+	$scope.groepsgenoten = [
+		{
+			naam: 'Milou Timmerman',
+			leeftijd: 19,
+			foto: 'img/hethoofd.jpg',
+			opleiding: {
+				naam: 'Geneeskunde',
+				jaar: 3
+			}
+		},
+		{
+			naam: 'Coen Calkhoven',
+			leeftijd: 21,
+			foto: 'img/hethoofd.jpg',
+			opleiding: {
+				naam: 'Biomedische Wetenschappen',
+				jaar: 2
+			}
+		},
+		{
+			naam: 'Dwayne Buurstede',
+			leeftijd: 25,
+			foto: 'img/hethoofd.jpg',
+			opleiding: {
+				naam: 'Geneeskunde',
+				jaar: 4
+			}
+		},
+		{
+			naam: 'Martijn Hols',
+			leeftijd: 24,
+			foto: 'img/hethoofd.jpg',
+			opleiding: {
+				naam: 'Geneeskunde',
+				jaar: 3
+			}
+		}
+	];
 });
