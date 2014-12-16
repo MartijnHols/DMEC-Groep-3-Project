843b6ec84018cc953ff40af68fb6833d455dc290
Martijn Hols <martijnhols@hotmail.com>
2014-12-16 13:46:24 +0100

    Implement groepsgenoten

--- js/expeditie.js
+++ js/expeditie.js
@@ -16,40 +16,57 @@ app.controller('main', function ($scope) {
 app.controller('groepsgenoten', function ($scope) {
 	$scope.groepsgenoten = [
 		{
-			naam: 'Milou Timmerman',
-			leeftijd: 19,
+			naam: 'Yvonne Geraats',
+			leeftijd: 22,
 			foto: 'img/hethoofd.jpg',
 			opleiding: {
 				naam: 'Geneeskunde',
 				jaar: 3
-			}
-		},
-		{
-			naam: 'Coen Calkhoven',
-			leeftijd: 21,
-			foto: 'img/hethoofd.jpg',
-			opleiding: {
-				naam: 'Biomedische Wetenschappen',
-				jaar: 2
-			}
-		},
-		{
-			naam: 'Dwayne Buurstede',
-			leeftijd: 25,
-			foto: 'img/hethoofd.jpg',
-			opleiding: {
-				naam: 'Geneeskunde',
-				jaar: 4
-			}
+			},
+			omschrijving: 'Yvonne is een geweldige fotografe die gestructureerd te werk gaat. Ze levert prachtige, creatieve en kwalitatieve fotografie en doet haar werk met een grote passie. Yvonne is altijd enthousiast en gedreven en staat ook altijd voor je klaar. Ik kijk uit naar verdere samenwerking met haar.'
 		},
 		{
 			naam: 'Martijn Hols',
 			leeftijd: 24,
-			foto: 'img/hethoofd.jpg',
+			foto: 'img/ik.jpg',
 			opleiding: {
 				naam: 'Geneeskunde',
 				jaar: 3
-			}
+			},
+			omschrijving: 'Ik ben een software ontwikkelaar met een passie voor web development. Vanuit deze passie ben ik al ruim 10 jaar (sinds 2004) aan het ontwikkelen.'
+		},
+		{
+			naam: 'Milou Timmerman',
+			leeftijd: 19,
+			foto: 'img/demilou.jpg',
+			opleiding: {
+				naam: 'Geneeskunde',
+				jaar: 3
+			},
+			omschrijving: 'Milou is een getalendeerde ontwerpster, met oog voor detail. Zo weet zij de juiste kleuren te combineren voor ieder project en iedere doelgroep, verder kan ze goede en professionele websites ontwerpen én bouwen! Ook maakt ze prachtige foto\'s en heeft ze ongelofelijk veel verstand van fotografie. Verder kan ze goed met klanten overleggen.'
+		},
+		{
+			naam: 'Coen Calkhoven',
+			leeftijd: 21,
+			foto: 'img/hetcoen.jpg',
+			opleiding: {
+				naam: 'Biomedische Wetenschappen',
+				jaar: 2
+			},
+			omschrijving: 'Coen, Coen, Coen, Coen. De een zegt held de andere honey, maar wij zeggen Coen.'
+		},
+		{
+			naam: 'Dwayne Buurstede',
+			leeftijd: 25,
+			foto: 'img/isdwayne.jpg',
+			opleiding: {
+				naam: 'Geneeskunde',
+				jaar: 4
+			},
+			omschrijving: 'Ontwikkeling van websites (webapps en mobiele websites) van begin tot eind, waaronder het overleg met klanten, design opleveren, programmeren en de daadwerkelijke oplevering. Hierbij ontwikkel ik mijzelf door steeds gebruik te maken van de meest recente webtechnieken.'
 		}
 	];
+	$('.box .content').niceScroll({
+		autohidemode: false
+	});
 });
